e4090f8b31f0392a8fe9153f1775ee6bb95b7472
Quang Ha <hakun32@gmail.com>
2022-03-29 22:08:16 +0700

    master-c2-29-03

--- app/src/main/java/com/example/football_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/football_kotlin/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.football_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+//        val button:Button = findViewById(R.id.btnClickme)
+//        button.setOnClickListener{
+//            val intent = Intent(this@MainActivity, DemoActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
